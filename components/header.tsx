b6dfded2ad81d8b778a63cdea20efa456f807920
hameedullah shaik <hameedsk381@gmail.com>
2025-01-24 13:13:02 +0530

    fixed bugs

--- components/header.tsx
+++ components/header.tsx
@@ -18,7 +18,7 @@ export default function HeaderContainner() {
     <header className="sticky top-0 z-20  bg-[hsl(var(--background))]">  
       <nav
         aria-label="Global"
-        className="mx-auto flex mx-6 sm:mx-16 items-center justify-between p-6 lg:px-8"
+        className="mx-auto flex  sm:mx-16 items-center justify-between p-6 lg:px-8"
       >
         <a href="#" className="-m-1.5 p-1.5">
           <span className="sr-only">Your Company</span>
