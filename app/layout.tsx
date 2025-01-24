a8dbce72e85803f3fddfd3d80cf887c0b73b9cfb
hameedullah shaik <hameedsk381@gmail.com>
2025-01-24 12:46:50 +0530

    new ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,3 @@
-import type { Metadata } from "next"
-import { Poppins } from "next/font/google"
-import "./globals.css"
-import clsx from 'clsx'
-import '../styles/tailwind.css'
-
-
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-  variable: "--font-poppins",
-})
-
-export const metadata: Metadata = {
-  title: "Uppraisal",
-  description: "Share your creations with the world",
-}
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en" className={clsx('antialiased', poppins.variable)}>
-      <body>{children}</body>
-    </html>
-  )
-}
-
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
