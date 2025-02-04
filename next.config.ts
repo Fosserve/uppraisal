e9acc43898ecb51c84c6aea724ee0b5eb3a7ca43
Praveen Gunja <gpraveen1542@gmail.com>
2025-02-04 18:19:09 +0530

    ui chnages

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['img.freepik.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.freepik.com',
+      },
+    ],
   },
 
 };
